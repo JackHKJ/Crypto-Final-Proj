ba67b53972d6f892c955024fef3b3b6aa3dad7e0
B14ck0ut <gavinhe00@gmail.com>
2020-08-01 21:34:10 -0700

    Jiahuan He

---
+++ Algorithms.py
@@ -0,0 +1,28 @@
+
+def to_binary(string):
+    temp = []
+    result = ""
+    for i in range(len(string)):
+        temp.append(bin(ord(string[i]))[2:9])
+    for i in range(len(temp)):
+        result += "0" * (8 - len(temp[i])) + temp[i]
+    return result
+
+
+def to_string(binary):
+    string = ""
+    for i in range(int(len(binary) / 8)):
+        string += chr(int(binary[i*8:i*8+8], 2))
+    return string
+
+
+def xor(a, b):
+    if len(a) != len(b):
+        print("ERROR: length not equal!")
+    temp = ""
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            temp += "0"
+        else:
+            temp += "1"
+    return temp
--- client.py
+++ client.py
@@ -1,25 +1,7 @@
 import socket
-
-
-def to_binary(string):
-    temp = []
-    result = ""
-    for i in range(len(string)):
-        temp.append(bin(ord(string[i]))[2:9])
-    for i in range(len(temp)):
-        result += "0" * (8 - len(temp[i])) + temp[i]
-    return result
-
-
-def to_string(binary):
-    string = ""
-    for i in range(int(len(binary) / 8)):
-        string += chr(int(binary[i*8:i*8+8], 2))
-    return string
-
+from Algorithms import *
 
 message = ""
-
 host = socket.gethostname()
 port = 8808
 while message != "exit":
